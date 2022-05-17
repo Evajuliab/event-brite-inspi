fb24993cbf3c8e71e3d7b1df6feb0472e31c6228
eva.laurent.78@gmail.com <eva@mbp-de-eva.home>
2022-05-17 10:26:56 +0200

    heroku push

---
+++ app/models/event.rb
@@ -0,0 +1,22 @@
+class Event < ApplicationRecord
+    validates :start_date, presence: true
+    validates :future_event
+    validates :duration, , presence: true,
+    numericality: { greater_than: 0 }
+    validates :title, presence: true, length: { in: 5..140 }
+    validate :price, presence: true, numericality: { in: 1..1000 }
+    validates :location, presence: true
+
+    belongs_to :user
+    has_many :attendances
+    has_many :users, through: :attendances
+end
+
+
+private
+
+def future_event
+  errors.add(:start_date, "Ne peut être dans le passé") if start_date < Time.now
+end
+end
+
