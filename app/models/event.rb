08c2d0d74dd897833485f1c41a15119becee9de6
eva.laurent.78@gmail.com <eva@mbp-de-eva.home>
2022-05-17 11:33:04 +0200

    bug fixes event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ApplicationRecord
     validates :start_date, presence: true
-    validate :future_event
+   
     validates :duration, presence: true, numericality: { greater_than: 0 }
     validates :title, presence: true, length: { in: 5..140 }
-    validate :price, presence: true, numericality: { in: 1..1000 }
+    validates :price, presence: true, numericality: { in: 1..1000 }
     validates :location, presence: true
 
     belongs_to :user
@@ -14,8 +14,5 @@ class Event < ApplicationRecord
 
 private
 
-def future_event
-  errors.add(:start_date, "Ne peut être dans le passé") if start_date < Time.now
-end
 
 end
